38c68d5c70b84f6753ac5d2c7f8565c9f20c83e5
Evan Lu <evanlu7@gmail.com>
2018-01-14 15:44:19 -0800

    setting up game functionality

--- game.js
+++ game.js
@@ -1,12 +1,44 @@
 var inquirer = require("inquerer");
 var chalk = require("chalk");
-var word = require ("./word");
-var words = require ("./words");
+var word = require("./word");
+var words = require("./words");
 
-function Game () {
-	var self = this;
-	this.play = function () {
-		this.guessesLeft = 10;
-		this.nextWord ();
-	};
+function Game() {
+    var self = this;
+    this.play = function() {
+        this.guessesLeft = 10;
+        this.nextWord();
+    };
+    this.pickWord = function() {
+        var chooseWord = words[Math.floor(Math.random() * words.length)];
+        this.currentWord = new Word(randomWord);
+        console.log(this.currentWord)
+        this.makeGuess();
+
+    };
+
+
+this.userGuess = function () {
+	this.userLetter().then(function() {
+		if (self.guessesLeft<1){
+			console.log(
+				"Game over! The answer was" + self.currentWord.getSolution());
+			self.askToPlayAgain();
+		}
+
+		else if (self.currentWord.guessedCorrectly()){
+			console.log("Correct.  Starting new game.");
+			self.guessesLeft = 10;
+			self.nextWord();
+		}
+		else{
+			self.makeGuess();
+		}
+	});
+};
+
+// play again function
+
+
+
 }
